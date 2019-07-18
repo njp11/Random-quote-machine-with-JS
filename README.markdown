5494502fe1b908705d0d802497efa9f742dab555
NiravPatel111 <48572469+NiravPatel111@users.noreply.github.com>
2019-07-18 15:19:16 -0400

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# Random quote machine
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/NiravP/pen/NVZpKq](https://codepen.io/NiravP/pen/NVZpKq).
+
+ 
---
+++ src/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  getQuote();
+  
+var randomQuote;
+var author;
+function getQuote(){
+  
+ let url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+  $.getJSON(url, function(data){
+     randomQuote = data.quoteText;
+    author = data.quoteAuthor;
+    $("#text").html(data.quoteText);
+    $("#author").html('- ' + data.quoteAuthor);
+  });
+
+};
+$("#tweet-quote").on("click", function(){
+  window.open("https://twitter.com/intent/tweet?text=" + randomQuote + '-' + author);
+});
+$("#new-quote").on("click", function(){
+  getQuote();
+});
+  
+});
+
